7bec66677713e7a7f4ddfb66c50f635154eef58b
Rémi <remi@signalwow.fr>
2020-04-08 15:22:51 +0200

    TouitAPI reconception. It's work well on build version

--- src/api/TouitAPI.js
+++ src/api/TouitAPI.js
@@ -1,21 +1,23 @@
+const TIMER = 1100;
+
 const TouitAPI = (callback) => {
-	setInterval(() => {
-		const myHeaders = new Headers();
+	const myHeaders = new Headers();
 
-		const myInit = {
-			method: 'GET',
-			headers: myHeaders,
-			mode: 'cors',
-			cache: 'default'
-		};
+	const myInit = {
+		method: 'GET',
+		headers: myHeaders,
+		mode: 'cors',
+		cache: 'default'
+	};
 
-		const myRequest = new Request('http://touiteur.cefim-formation.org/list',myInit);
+	const myRequest = new Request('http://touiteur.cefim-formation.org/list',myInit);
 
-		fetch(myRequest,myInit)
-			.then(response => response.json())
-			.then(json => callback(json));
-
-	}, 1000);
+	fetch(myRequest,myInit)
+		.then(response => response.json())
+		.then(json => {
+			callback(json);
+			setTimeout(this, TIMER * 1.5);
+		});
 }
 
 export default TouitAPI;
